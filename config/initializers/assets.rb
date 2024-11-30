280fcbebc2da13cc577d0a68ec45f18e05842141
Tayaska <tajanmisiura@gmail.com>
2024-11-30 17:15:04 +0200

    Updating and connecting styles

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,8 +1,15 @@
 # Be sure to restart your server when you modify this file.
 
 # Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+# config/initializers/assets.rb
+
+Rails.application.config.assets.paths += [
+  Rails.root.join("app", "assets", "stylesheets")
+]
+
+# Додати SCSS файли до precompile, якщо необхідно
 Rails.application.config.assets.precompile += %w( application.scss )
+
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
